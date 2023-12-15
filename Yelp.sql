c861c9a121f7b772a19e6e822cab796cd51fa0c7
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-12-15 00:14:53 -0600

    added queries to find number of records for each table

---
+++ Yelp.sql
@@ -0,0 +1,48 @@
+
+--1. Profiling
+
+--Attribute Records
+Select *
+From attribute
+
+--Business Records
+Select *
+From business
+
+--Category Records
+Select *
+From category
+
+--Checkin Records 
+Select *
+From checkin
+
+--Elite_Years Records
+Select *
+From elite_years
+
+--Friend Records
+Select *
+From friend
+
+--Hours Records 
+Select *
+From hours
+
+--Photo Records
+Select *
+From photo
+
+--Review Records 
+Select *
+From review
+
+--Tip Records
+Select *
+From tip
+
+--User Records
+Select *
+From user
+
+
